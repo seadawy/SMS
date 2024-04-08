06a22a5d1290f98b0f72015c115985edee78da85
abdelrhman osama seadawy <52138397+seadawy@users.noreply.github.com>
2024-04-08 16:25:38 +0200

    Update and rename Managercontroller.php to ManagerController.php

--- app/Http/Controllers/Admin/ManagerController.php
+++ app/Http/Controllers/Admin/ManagerController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class Managercontroller extends Controller
+class ManagerController extends Controller
 {
     public function dashboard()
     {
